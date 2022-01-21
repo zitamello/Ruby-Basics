6e1f50c737f2eef96e679571d728feffd26e83c8
Zita Mello <zitamello@gmail.com>
2022-01-21 08:24:29 -0300

    practicing getting input from user

--- getinputfromuser.rb
+++ getinputfromuser.rb
@@ -1,3 +1,11 @@
-puts "What's your name?"
+puts "Enter your first name"
 name = gets.chomp
-puts "Ok, so your name is #{name}... Nice to meet you!"
+
+puts "Enter your last name"
+lastname = gets.chomp
+
+fullname = name + lastname
+
+puts "Your full name is #{name} #{lastname}"
+puts "Your full name reversed is #{lastname.reverse} #{name.reverse}"
+puts "Your name has a total of #{fullname.length} characters in it"
