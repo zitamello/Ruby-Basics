c80f11d9db4ac816c016634d65bc627d85ebccc0
Zita Mello <zitamello@gmail.com>
2022-01-25 10:17:26 -0300

    changing crud.rb file to make module Crud

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -1,46 +1,31 @@
-require 'bcrypt'
 
-users = [
-          { username: "jon", password: "ghost" },
-          { username: "robb", password: "greywind"},
-          { username: "sansa", password: "lady"},
-          { username: "arya", password: "nymeria"},
-          { username: "brandon", password: "summer"},
-          { username: "rickson", password: "shaggydog"}
-        ]
+module Crud
+  require 'bcrypt'
 
-#generating hash for user's password
-def generate_hash_for_passwords(user_password)
-  BCrypt::Password.create(user_password)
-end
-
-def verification_hash(password)
-  BCrypt::Password.new(password)
-end
-
-def create_security_for_users(users)
-  users.each do |user_record|
-    user_record[:password] = generate_hash_for_passwords(user_record[:password])
+  #generating hash for user's password
+  def generate_hash_for_passwords(user_password)
+    BCrypt::Password.create(user_password)
   end
-  users
-end
 
-new_users = create_security_for_users(users)
+  def verification_hash(password)
+    BCrypt::Password.new(password)
+  end
 
-def authenticate_user(entry_username, entry_password, new_users)
-  new_users.each do |user_record|
-    if user_record[:username] == entry_username && verification_hash(user_record[:password]) == entry_password
-      return user_record
+  def create_security_for_users(users)
+    users.each do |user_record|
+      user_record[:password] = generate_hash_for_passwords(user_record[:password])
     end
+    users
   end
-  "Invalid Credentials. Your username or password is not correct."
+
+  new_users = create_security_for_users(users)
+
+  def authenticate_user(entry_username, entry_password, new_users)
+    new_users.each do |user_record|
+      if user_record[:username] == entry_username && verification_hash(user_record[:password]) == entry_password
+        return user_record
+      end
+    end
+    "Invalid Credentials. Your username or password is not correct."
+  end
 end
-
-puts "Enter your username: "
-entry_username = gets.chomp
-
-puts "Enter your password: "
-entry_password = gets.chomp
-
-
-p authenticate_user(entry_username, entry_password, new_users)
