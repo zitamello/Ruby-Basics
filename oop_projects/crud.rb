a9d305d1e413aa8e8cd446bc14209c46a50d2d95
Zita Mello <zitamello@gmail.com>
2022-01-25 09:35:46 -0300

    generate hash methods for passwords

--- oop_projects/crud.rb
+++ oop_projects/crud.rb
@@ -1,11 +1,28 @@
 require 'bcrypt'
 
+users = [
+          { username: "jon", password: "ghost" },
+          { username: "robb", password: "greywind"},
+          { username: "sansa", password: "lady"},
+          { username: "arya", password: "nymeria"},
+          { username: "brandon", password: "summer"},
+          { username: "rickson", password: "shaggydog"}
+        ]
+
 #generating hash for user's password
 def generate_hash_for_passwords(user_password)
   BCrypt::Password.create(user_password)
 end
 
-puts "Enter your password: "
-user_password = gets.chomp
-hashed_password = generate_hash_for_passwords(user_password)
-puts hashed_password
+def verification_hash(password)
+  BCrypt::Password.new(password)
+end
+
+def create_security_for_users(users)
+  users.each do |user_record|
+    user_record[:password] = generate_hash_for_passwords(user_record[:password])
+  end
+  users
+end
+
+puts create_security_for_users(users)
