c094d024f9348249424a2bb2a31cad62a4d2dda9
Zita Mello <zitamello@gmail.com>
2022-01-25 08:30:59 -0300

    student.rb updated

--- oop_projects/student.rb
+++ oop_projects/student.rb
@@ -1,7 +1,33 @@
 class Student
-  @first_name
-  @last_name
-  @email
-  @username
-  @password 
+  attr_accessor :first_name, :last_name, :email, :username, :password
+
+  def initialize(firstname, lastname, email, username, password)
+    @first_name = firstname
+    @last_name = lastname
+    @email = email
+    @username = username
+    @password = password
+  end
+
+  def to_s
+    "First name: #{@first_name}"
+    "Last name: #{@last_name}"
+    "Email : #{@email}"
+    "Username: #{@username}"
+  end
+
+  def set_username
+    @username = "alexhossain123"
+  end
 end
+
+
+mashrur = Student.new()
+mashrur.first_name = "Alex"
+mashrur.last_name = "Hossain"
+mashrur.email = "Alexhossain@gmail.com"
+
+puts mashrur.first_name
+puts mashrur.last_name
+puts mashrur.email
+puts mashrur.set_username
